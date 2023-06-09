352c8e4e801dae71997d1f8e0d900ab91397dc6e
andra <andras_kapitany@hotmail.hu>
2023-06-09 21:29:38 +0200

    constructor-homework done

--- BankAccount.java
+++ BankAccount.java
@@ -1,3 +1,49 @@
 public class BankAccount {
     // TODO
+    private String accountNumber;
+    private String accountHolder;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolder, double balance){
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+    public BankAccount(){
+        this("123456","Lámpás Józsi",7894561);
+
+
+    }
+
+    public BankAccount(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+    public BankAccount(double balance){
+        this.balance = balance;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public String getAccountHolder() {
+        return this.accountHolder;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
 }
--- Main.java
+++ Main.java
@@ -5,5 +5,30 @@ public class Main {
 
         Car defaultCar = new Car();
         System.out.println(defaultCar.getModel()); // Output: Unknown
+
+
+        BankAccount accountOne = new BankAccount();
+        BankAccount accountTwo = new BankAccount("6547897","Short Top", 84779.14);
+        System.out.println();
+//        accountOne.setAccountNumber("639325821471");
+//        accountTwo.setAccountNumber("147125883699");
+//        accountOne.setAccountHolder("Végh Béla");
+//        accountTwo.setAccountHolder("Zöldes Barna");
+//        accountOne.setBalance(52136.89);
+//        accountTwo.setBalance(74581.98);
+////
+//        System.out.println("Account holder name : " + accountOne.getAccountHolder() +
+//                " Account number: " + accountOne.getAccountNumber()
+//        + " Balance: " + accountOne.getBalance());
+//        System.out.println("Account holder name : " + accountTwo.getAccountHolder() +
+//                " Account number: " + accountTwo.getAccountNumber()
+//                + " Balance: " + accountTwo.getBalance());
+        System.out.println("Account number: " + accountOne.getAccountNumber() +
+                " Name : " + accountOne.getAccountHolder()
+                + " Balance : " + accountOne.getBalance());
+        System.out.println("Account number: " + accountTwo.getAccountNumber() +
+                " Name : " + accountTwo.getAccountHolder()
+                + " Balance : " + accountTwo.getBalance());
+
     }
 }
